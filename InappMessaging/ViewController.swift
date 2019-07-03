053478f098ada6fc3c07852c3aa80633d96c79cf
Paul Ardeleanu <paul.ardeleanu@vonage.com>
2019-07-03 11:12:02 +0100

    Initial Commit

---
+++ InappMessaging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InappMessaging
+//
+//  Created by Paul Ardeleanu on 03/07/2019.
+//  Copyright © 2019 Nexmo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
